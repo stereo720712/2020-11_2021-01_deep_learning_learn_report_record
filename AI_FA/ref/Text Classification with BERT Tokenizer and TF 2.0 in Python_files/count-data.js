f32039cc5ce94ad428ff7a2079977d8b545f0ff8
Sean <stereo720130@gmail.com>
2021-01-07 11:38:06 +0800

    update

---
+++ AI_FA/ref/Text Classification with BERT Tokenizer and TF 2.0 in Python_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"https:\/\/stackabuse.com\/text-classification-with-bert-tokenizer-and-tf-2-0-in-python\/","comments":19}]});
+}
